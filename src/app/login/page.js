936787ae25f14ee7a9357770da9912a6e7fc1a29
KELLY <kmcartwright@yahoo.co.uk>
2024-03-07 14:40:43 +0000

    params

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 
-export default function LoginPage({ onLogin }) {
+export default function LoginPage({ params }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
