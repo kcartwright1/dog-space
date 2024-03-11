e4ac0195d9f9f24a19d63591d1d9cef6dd3e68ca
KELLY <kmcartwright@yahoo.co.uk>
2024-03-11 10:37:26 +0000

    error fix

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -5,14 +5,14 @@ export default function DashboardPage() {
     <div className="dashcontainer">
       <div className="dash-content-bodyTop">
         <div>
-          <h1>Hello, Welcome to your pawfect post feed!</h1>
+          <h1>Hello Welcome to your pawfect post feed</h1>
         </div>
         <div className="dash-content-bodyBottom">
           <div className="mytimeline">
             <h2>My Timeline</h2>
           </div>
           <div className="friendsposts">
-            <h2>My Fur Friend's Posts</h2>
+            <h2>My Fur Friends Posts</h2>
           </div>
           <SignOutButton></SignOutButton>
         </div>
